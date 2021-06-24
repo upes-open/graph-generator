48801607ada002c0e0b8cc3aa1e091b70801036d
Abhijeet Prasad <abhi.prasad16@gmail.com>
2021-06-24 15:27:29 +0530

    Initial project setup with Maven

---
+++ src/main/java/org/upes-open/graph-generator/App.java
@@ -0,0 +1,13 @@
+package org.upes-open.graph-generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
